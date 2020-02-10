ec1b9cc954319377e871cdb1725bbf216f0db701
Robin Gurbuz <robin.gurbuz.0439@student.uu.se>
2020-02-10 17:21:18 +0100

    5-2

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,36 +2,36 @@ let food = [
     {
         "name": "The Fire Burger",
         "calories": 2000,
-        "gluten": true,
-        "lactose": true,
+        "gluten": "Yes",
+        "lactose": "Yes",
         "img": "https://wallpapershome.com/images/pages/pic_v/16604.jpg"
     },
     {
         "name": "Fried Turkey Burger",
         "calories": 500,
-        "gluten": false,
-        "lactose": true,
+        "gluten": "No",
+        "lactose": "Yes",
         "img": "https://d5dnlg5k9nac9.cloudfront.net/processed/thumbs/8ee5c88bc69f40691e73d9f68045b813c9268bef_r791_530.png"
     },
     {
         "name": "A Double w/ Cheese",
         "calories": 1200,
-        "gluten": true,
-        "lactose": true,
+        "gluten": "Yes",
+        "lactose": "Yes",
         "img": "https://lh3.googleusercontent.com/Ele8gtDOd1tlQ4TebRJkf6XgpciDD2e57Jf5E3UwAzvfPs0kLhOXJLewZYsu1CFn9CPly0TLvrZUCqJnXMn0=s525"
     },
     {
         "name": "Vegan Burger",
         "calories": 300,
-        "gluten": false,
-        "lactose": false,
+        "gluten": "No",
+        "lactose": "No",
         "img": "https://sweetsimplevegan.com/wp-content/uploads/2019/06/Easy-5-Ingredient-Veggie-Burgers-Sweet-Simple-Vegan-thumbnail.jpg"
     },
     {
         "name": "Bacon Burger",
         "calories": 5000,
-        "gluten": false,
-        "lactose": true,
+        "gluten": "Yes",
+        "lactose": "Yes",
         "img": "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/0_t9wcri33/def_height/2700/def_width/2700/version/100012/type/1"
     }
 ]
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -22,20 +22,16 @@ let baconBurger = new menuItem('Bacon Burger', '5000', 'Yes', 'Yes', 'https://pr
 let menu = [fireBurger, turkeyBurger, doubleCheese, vegBurger, baconBurger];
 */
 
-/* jslint es6:true, indent: 2 */
-/* global Vue, io */
-/* exported vm */
-
 'use strict';
 const socket = io();
 
-const vm1 = new Vue({
+const vm = new Vue({
 	el: '#burgerList',
   data: {
       menu: food
       //menu: menu
   }
-});
+})
 
 const vm2 = new Vue({
     el: '#info_box',
@@ -46,8 +42,8 @@ const vm2 = new Vue({
         burgerArray: [],
         fullname: '',
         email: '',
-        //street: '',
-        //house: '',
+        street: '',
+        house: '',
         picked: 'Not provided',
         selected: 'Credit card',
         options: [
@@ -58,27 +54,27 @@ const vm2 = new Vue({
         ]
     },
     created: function() {
-    /* When the page is loaded, get the current orders stored on the server.
-     * (the server's code is in app.js) */
-    socket.on('initialize', function(data) {
-      this.orders = data.orders;
-    }.bind(this));
-
-    /* Whenever an addOrder is emitted by a client (every open map.html is
-     * a client), the server responds with a currentQueue message (this is
-     * defined in app.js). The message's data payload is the entire updated
-     * order object. Here we define what the client should do with it.
-     * Spoiler: We replace the current local order object with the new one. */
-    socket.on('currentQueue', function(data) {
-      this.orders = data.orders;
-    }.bind(this));
-  },
+        /* When the page is loaded, get the current orders stored on the server.
+         * (the server's code is in app.js) */
+        socket.on('initialize', function(data) {
+            this.orders = data.orders;
+        }.bind(this));
+        
+        /* Whenever an addOrder is emitted by a client (every open map.html is
+         * a client), the server responds with a currentQueue message (this is
+         * defined in app.js). The message's data payload is the entire updated
+         * order object. Here we define what the client should do with it.
+         * Spoiler: We replace the current local order object with the new one. */
+        socket.on('currentQueue', function(data) {
+            this.orders = data.orders;
+        }.bind(this));
+    },
     methods: {
-        getFormValues: function() {
+        getFormValues () {
             this.submitted = true;
             this.getBurgers();
         },
-        getBurgers: function() {
+        getBurgers () {
             let burgerBoxes = document.getElementsByName('selectBurger');
             for(var box of burgerBoxes){
                 if(box.checked){
@@ -90,30 +86,29 @@ const vm2 = new Vue({
       /* This function returns the next available key (order number) in
        * the orders object, it works under the assumptions that all keys
        * are integers. */
-            let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
-                return Math.max(last, next);
-            }, 0);
-            return lastOrder + 1;
+      let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+        return Math.max(last, next);
+      }, 0);
+      return lastOrder + 1;
+    },
+    addOrder: function(event) {
+      /* When you click in the map, a click event object is sent as parameter
+       * to the function designated in v-on:click (i.e. this one).
+       * The click event object contains among other things different
+       * coordinates that we need when calculating where in the map the click
+       * actually happened. */
+      let offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top,
+      };
+      socket.emit('addOrder', {
+        orderId: this.getNext(),
+        details: {
+          x: event.clientX - 10 - offset.x,
+          y: event.clientY - 10 - offset.y,
         },
-        addOrder: function(event) {
-            /* When you click in the map, a click event object is sent as parameter
-             * to the function designated in v-on:click (i.e. this one).
-             * The click event object contains among other things different
-             * coordinates that we need when calculating where in the map the click
-             * actually happened. */
-            let offset = {
-                x: event.currentTarget.getBoundingClientRect().left,
-                y: event.currentTarget.getBoundingClientRect().top,
-            };
-            socket.emit('addOrder', {
-                orderId: this.getNext(),
-                details: {
-                    x: event.clientX - 10 - offset.x,
-                    y: event.clientY - 10 - offset.y,
-                },
-                orderItems: ['Beans', 'Curry'],
-            });
-        },
+        orderItems: ['Beans', 'Curry'],
+      });
+    },
     }
-    
-});
+})
